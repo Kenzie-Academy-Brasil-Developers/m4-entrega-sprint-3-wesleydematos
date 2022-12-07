20ed6d1c0a3f2a8b7b70d91ee30df51e89c46b86
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-07 15:39:43 -0300

    feat: add routes to app.use

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import express from "express";
+import categoriesRoutes from "./routers/categoriesRoutes";
+import productsRoutes from "./routers/productsRoutes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/categories", categoriesRoutes);
+app.use("/products", productsRoutes);
 
 export default app;
