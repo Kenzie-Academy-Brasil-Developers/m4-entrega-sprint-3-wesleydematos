336b5f236cea6fdd4bec92100c80721b440e1a3f
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-08 13:05:13 -0300

    feat: make createCategoryService functional

--- src/services/categories/createCategoryService.js
+++ src/services/categories/createCategoryService.js
@@ -1,6 +1,27 @@
-const createCategoryService = async (req) => {
-  console.log(req);
-  return [200, {}];
+import database from "../../database";
+import { createCategorySerializer } from "../../serializers/categoriesSerializers";
+
+const createCategoryService = async (categoryData) => {
+  try {
+    const validated = await createCategorySerializer.validate(categoryData, {
+      stripUnknown: true,
+    });
+
+    const queryResponse = await database.query(
+      `INSERT INTO
+        categories(name)
+      VALUES
+        ($1)
+      RETURNING *;`,
+      [validated.name]
+    );
+
+    return [201, queryResponse.rows[0]];
+  } catch (error) {
+    console.log(error.errors);
+
+    return [400, { message: error.errors }];
+  }
 };
 
 export default createCategoryService;
