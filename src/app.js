a67ba12c8997090c336c2a4219d342efa1b1f10a
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-12-11 19:31:29 -0300

    feat: add express-async-errors and create class AppError

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
+import "express-async-errors";
 import express from "express";
 import categoriesRoutes from "./routers/categoriesRoutes";
 import productsRoutes from "./routers/productsRoutes";
+import { errorHandler } from "./errors";
 
 const app = express();
 
 app.use(express.json());
 app.use("/categories", categoriesRoutes);
 app.use("/products", productsRoutes);
+app.use(errorHandler);
 
 export default app;
